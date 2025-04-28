71a2cb7d3c07d4f819c117eb4a86f93cc4c81edf
zahidjack <zahidnaseem1717@gmail.com>
2025-04-28 14:17:38 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function startCheckout(productName, price, sizeId) {
-    document.getElementById('checkoutForm').classList.remove('hidden');
-    window.scrollTo(0, document.body.scrollHeight);
+function orderNow(productName) {
+  document.getElementById('order-form').scrollIntoView({ behavior: "smooth" });
 }
 
-document.getElementById('orderForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Generate Random Order Number
-    const orderNum = 'PK' + Math.floor(100000 + Math.random() * 900000);
-
-    // Hide Form, Show Success
-    document.getElementById('checkoutForm').classList.add('hidden');
-    document.getElementById('orderSuccess').classList.remove('hidden');
-    document.getElementById('orderNumber').textContent = orderNum;
+document.getElementById('shippingForm').addEventListener('submit', function(event){
+  event.preventDefault();
+  
+  const orderNumber = 'PK-' + Math.floor(100000 + Math.random() * 900000);
+  document.getElementById('generatedOrder').textContent = orderNumber;
+  document.getElementById('orderNumber').style.display = 'block';
 });
